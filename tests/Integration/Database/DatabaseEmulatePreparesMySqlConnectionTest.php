b05c02b566e5c393ee790b2f95f9789f3fb7d4d4
Dries Vints <dries.vints@gmail.com>
2020-02-24 10:11:54 +0100

    Use root user again

--- tests/Integration/Database/DatabaseEmulatePreparesMySqlConnectionTest.php
+++ tests/Integration/Database/DatabaseEmulatePreparesMySqlConnectionTest.php
@@ -2,11 +2,16 @@
 
 namespace Illuminate\Tests\Integration\Database;
 
+use PDO;
+
 class DatabaseEmulatePreparesMySqlConnectionTest extends DatabaseMySqlConnectionTest
 {
     protected function getEnvironmentSetUp($app)
     {
         $app['config']->set('app.debug', 'true');
         $app['config']->set('database.default', 'mysql');
+        $app['config']->set('database.connections.mysql.options', [
+            PDO::ATTR_EMULATE_PREPARES => true,
+        ]);
     }
 }
