753f158eed87873b7cc002faa80aa196f9c35d56
Dries Vints <dries.vints@gmail.com>
2020-02-24 10:06:44 +0100

    Cleanup

--- tests/Integration/Database/DatabaseEmulatePreparesMySqlConnectionTest.php
+++ tests/Integration/Database/DatabaseEmulatePreparesMySqlConnectionTest.php
@@ -7,21 +7,6 @@ class DatabaseEmulatePreparesMySqlConnectionTest extends DatabaseMySqlConnection
     protected function getEnvironmentSetUp($app)
     {
         $app['config']->set('app.debug', 'true');
-
-        // Database configuration
-        $app['config']->set('database.default', 'testbench');
-
-        $app['config']->set('database.connections.testbench', [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'username' => 'root',
-            'password' => 'password',
-            'database' => 'forge',
-            'prefix' => '',
-            'options' => [
-                \PDO::ATTR_EMULATE_PREPARES => true,
-            ],
-        ]);
+        $app['config']->set('database.default', 'mysql');
     }
 }
--- tests/Integration/Database/DatabaseMySqlConnectionTest.php
+++ tests/Integration/Database/DatabaseMySqlConnectionTest.php
@@ -18,19 +18,7 @@ class DatabaseMySqlConnectionTest extends TestCase
     protected function getEnvironmentSetUp($app)
     {
         $app['config']->set('app.debug', 'true');
-
-        // Database configuration
-        $app['config']->set('database.default', 'testbench');
-
-        $app['config']->set('database.connections.testbench', [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'username' => 'root',
-            'password' => 'password',
-            'database' => 'forge',
-            'prefix' => '',
-        ]);
+        $app['config']->set('database.default', 'mysql');
     }
 
     protected function setUp(): void
